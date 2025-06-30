3311478cb779b7f58764c716f078b630469168fe
ppabis <piotr@pabis.eu>
2025-06-30 06:30:03 +0200

    Remove master user secret ARN output from PostgreSQL configuration

--- postgresql/outputs.tf
+++ postgresql/outputs.tf
@@ -2,9 +2,3 @@ output "db_endpoint" {
   description = "RDS Instance Endpoint"
   value       = aws_db_instance.postgresql.endpoint
 }
-
-output "master_user_secret_arn" {
-  description = "ARN of the master user secret in Secrets Manager"
-  value       = aws_db_instance.postgresql.master_user_secret[0].secret_arn
-  sensitive   = true
-} 
