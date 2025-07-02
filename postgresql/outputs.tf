cd502fd22509babcf4082b066d269b47560a3cd0
ppabis <piotr@pabis.eu>
2025-07-02 19:51:48 +0200

    Add DB ARN outputs

--- postgresql/outputs.tf
+++ postgresql/outputs.tf
@@ -2,3 +2,7 @@ output "db_endpoint" {
   description = "RDS Instance Endpoint"
   value       = aws_db_instance.postgresql.endpoint
 }
+
+output "db_arn" {
+  value = aws_db_instance.postgresql.arn
+}
