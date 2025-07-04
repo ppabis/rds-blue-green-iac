cdd0dd0600e89d291799246b9f028a00a3898988
ppabis <piotr@pabis.eu>
2025-07-04 20:53:47 +0200

    A complete script to deploy

--- apply.sh
+++ apply.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+export AWS_DEFAULT_REGION=eu-west-2
+
 tofu apply -target module.vpc.aws_secretsmanager_secret_version.root_password
 tofu apply
+aws secretsmanager get-secret-value \
+ --secret-id $(tofu output -raw secret_arn) \
+ --query SecretString \
+ --output text \
+ | jq -r ".password"
+aws ssm start-session \
+  --target $(tofu output -raw instance_id)
