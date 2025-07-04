3e72be9ebb28ff21df263b6dbae8980386855020
ppabis <piotr@pabis.eu>
2025-07-04 20:53:32 +0200

    Expose outputs for instance and secret

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,15 @@ output "postgresql_db_endpoint" {
 output "mysql_db_endpoint" {
   value = aws_cloudformation_stack.mysql-database.outputs["DBEndpoint"]
 }
+
+output "instance_id" {
+  value = module.vpc.instance_id
+}
+
+output "secret_arn" {
+  value = module.vpc.root_password_arn
+}
+
+output "instance_public_dns" {
+  value = module.vpc.instance_public_dns
+}
